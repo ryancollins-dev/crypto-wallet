fd63324f7cb2647e8b48f43609d9d6b777f88db1
ryancollins-dev <email@ryd3v.rocks>
2022-10-18 00:11:45 -0400

    add web3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import './App.css';
-
 import { Layout } from 'antd';
+import Web3 from 'web3';
+const web3 = new Web3('ws://localhost:7545');
 const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
+  console.log(web3);
   return (
     <div className='App'>
       <Layout>
