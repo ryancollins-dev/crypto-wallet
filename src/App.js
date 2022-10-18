2691c965240eaa783661424c5f86d3e9de3d37c5
ryancollins-dev <email@ryd3v.rocks>
2022-10-18 00:23:08 -0400

    node info header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Web3 from 'web3';
+
 import 'antd/dist/antd.min.css';
 import './App.css';
 import { Layout } from 'antd';
-import Web3 from 'web3';
-const web3 = new Web3('ws://localhost:7545');
 const { Header, Footer, Sider, Content } = Layout;
 
+const web3 = new Web3('ws://localhost:7545');
+
 function App() {
-  console.log(web3);
+  const [node, setNode] = useState('Unknown Node');
+
+  web3.eth.getNodeInfo(function (error, result) {
+    if (error) {
+      console.error(error);
+    } else {
+      setNode(result);
+    }
+  });
+
   return (
     <div className='App'>
       <Layout>
-        <Header>Header</Header>
+        <Header>{node}</Header>
         <Layout>
           <Sider>Sider</Sider>
           <Content>Content</Content>
