bb0134ee6dea3f9cc3363e6bf840f67d014ad3fc
ryancollins-dev <email@ryd3v.rocks>
2022-10-18 16:28:10 -0400

    add balance view

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Web3 from 'web3';
 
 import 'antd/dist/antd.min.css';
 import './App.css';
-import { Layout, Tree } from 'antd';
+import { Layout, Tree, Statistic } from 'antd';
 
 const { Header, Footer, Sider, Content } = Layout;
 const { TreeNode } = Tree;
@@ -12,6 +12,7 @@ const web3 = new Web3('ws://localhost:7545');
 function App() {
   const [node, setNode] = useState('Unknown Node');
   const [accounts, setAccounts] = useState([]);
+  const [balance, setBalance] = useState(0);
 
   web3.eth.getNodeInfo(function (error, result) {
     if (error) {
@@ -51,13 +52,25 @@ function App() {
     return 'Noname';
   };
 
+  const onSelectAccount = (keys) => {
+    const [account] = keys;
+
+    if (account && account !== 'accounts') {
+      web3.eth.getBalance(account).then(function (result) {
+        setBalance(web3.utils.fromWei(result, 'ether'));
+      });
+    } else {
+      setBalance(0);
+    }
+  };
+
   return (
     <div className='App'>
       <Layout>
         <Header>{node}</Header>
         <Layout>
           <Sider>
-            <Tree>
+            <Tree onSelect={onSelectAccount}>
               <TreeNode title='Accounts' key='accounts'>
                 {accounts.map((account) => (
                   <TreeNode
@@ -68,7 +81,13 @@ function App() {
               </TreeNode>
             </Tree>
           </Sider>
-          <Content>Content</Content>
+          <Content>
+            <Statistic
+              title='Account Balance (Eth)'
+              value={balance}
+              precision={2}
+            />
+          </Content>
         </Layout>
         <Footer>Footer</Footer>
       </Layout>
